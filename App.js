13ba1f5968c409c974e919873b39f7cda1f40466
mahithsc <mahith.chitrapu@gmail.com>
2022-02-15 18:03:58 -0500

    completly configuring lottie view

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import LottieView from 'lottie-react-native';
 import { FAB, Overlay } from 'react-native-elements';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
+import { GREY } from './assets/constants';
 
 export default function App() {
 
+  const [lottieVisible, changeLottieVisible] = useState(true)
+
   const [visible, changeVisible] = useState(false)
+  const [activity, changeActivity] = useState("")
 
   const toggle = () => {
     changeVisible(!visible)
@@ -16,38 +20,45 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <LottieView
-        source={require('./assets/51382-astronaut-light-theme.json')}
-        loop
-        autoPlay
-        style={{
-          width: '100%'
-        }}
-      />
+      {
+        lottieVisible ? <LottieView
+          source={require('./assets/51382-astronaut-light-theme.json')}
+          loop
+          autoPlay
+          style={{
+            width: '100%'
+          }}
+        /> : null
+      }
       <FAB
-      onPress={toggle}
+        onPress={toggle}
         placement="right"
         icon={() => (
           <AntDesign name="plus"
-            size={24} color="black" />
+            size={25} color="black" />
         )}
         color="white"
         style={{
-          margin: 30
+          margin: 25,
         }}
       />
 
-      <Overlay isVisible={visible} onBackdropPress={() => {
-        changeVisible(!visible)}}>
-        <Text style={styles.textPrimary}>Hello!</Text>
-        <Text style={styles.textSecondary}>
-          Welcome to React Native Elements
-        </Text>
-        <Button
-          title="Start Building"
+      <Overlay
+        isVisible={visible}
+        onBackdropPress={() => { changeVisible(!visible) }}
+        overlayStyle={{ backgroundColor: GREY, flex: 1, marginVertical: '70%', alignSelf: 'stretch', marginHorizontal: '10%', borderRadius: 10 }}
+      >
+        <Text>Enter your activity</Text>
+        <TextInput
+          value={activity}
+          onChangeText={changeActivity}
+          autoCorrect={false}
+          style={{
+            height: 20,
+            borderWidth: 1
+          }}
         />
       </Overlay>
-
     </View>
   );
 }
@@ -55,7 +66,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
