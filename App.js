24a26a08d797fbcba52a4af2dc3c011cbada671c
mahithsc <mahith.chitrapu@gmail.com>
2022-02-15 17:35:53 -0500

    added the popup and FAB

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import LottieView from 'lottie-react-native';
-import { FAB } from 'react-native-elements';
-import React from 'react';
+import { FAB, Overlay } from 'react-native-elements';
+import React, {useState} from 'react';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
-  const [visible, setVisible] = React.useState(true);
+
+  const [visible, changeVisible] = useState(false)
+
+  const toggle = () => {
+    changeVisible(!visible)
+  }
 
   return (
     <View style={styles.container}>
@@ -15,23 +20,34 @@ export default function App() {
         source={require('./assets/51382-astronaut-light-theme.json')}
         loop
         autoPlay
-        style = {{
+        style={{
           width: '100%'
         }}
       />
       <FAB
-          visible={true}
-          onPress={() => setVisible(!visible)}
-          placement="right"
-          icon={() => (
-            <AntDesign name="plus" 
+      onPress={toggle}
+        placement="right"
+        icon={() => (
+          <AntDesign name="plus"
             size={24} color="black" />
-          )}
-          color="white"
-          style = {{
-            margin: 30
-          }}
+        )}
+        color="white"
+        style={{
+          margin: 30
+        }}
+      />
+
+      <Overlay isVisible={visible} onBackdropPress={() => {
+        changeVisible(!visible)}}>
+        <Text style={styles.textPrimary}>Hello!</Text>
+        <Text style={styles.textSecondary}>
+          Welcome to React Native Elements
+        </Text>
+        <Button
+          title="Start Building"
         />
+      </Overlay>
+
     </View>
   );
 }
@@ -42,5 +58,18 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    margin: 10,
+  },
+  textPrimary: {
+    marginVertical: 20,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  textSecondary: {
+    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 17,
   },
 });
